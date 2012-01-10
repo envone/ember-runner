ed260c92050559761786196fd9a07c7fbf045dc5
juan77 <jpgoldfinger@gmail.com>
2012-01-09 22:43:30 -0300

    Added task:checkPackages, task:checkConfig Now merge dev's ember_runner_config with the original one removed logs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var buildInfo, workFiles;
 
 // Clean args.
 args.shift(); // node
-args.shift(); // ember-runtime
+args.shift(); // ember-runner
 
 runner.task('default', 'Run preview server', ['preview'], function(callback) {
   callback(null, true);
@@ -30,7 +30,7 @@ runner.task('watch', 'Run preview server', ['build'], function(callback) {
   });
 });
 
-runner.task('build', 'Build libraries and applications', ['task:configure', 'task:clean', 'task:walk', 'vendors', 'apps'], function(callback) {
+runner.task('build', 'Build libraries and applications', ['task:configure', 'task:clean', 'vendors', 'apps', 'task:checkPackages', 'task:walk'], function(callback) {
   pm.build(function(err, success) {
     var distInfos = pm.distributions,
         pending = distInfos.length;
@@ -59,8 +59,8 @@ runner.task('apps', 'Generate applications libraries', function(callback) {
       distPackages.push(pm.createPackage({
         isApp: true,
         name: name,
-        //distribution: dist,
         path: [buildInfo.srcApps, pack].join('/'),
+        smallPath: pack,
         styles: apps.styles,
         static: apps.static,
         templates: apps.templates,
@@ -69,6 +69,7 @@ runner.task('apps', 'Generate applications libraries', function(callback) {
     });
 
     pm.createDistInfo({
+      isApp: true,
       name: dist,
       packages: distPackages,
       output: [buildInfo.apps.output, dist].join('/')
@@ -94,8 +95,8 @@ runner.task('vendors', 'Generate vendors libraries', function(callback) {
       distPackages.push(pm.createPackage({
         isVendor: true,
         name: name,
-        //distribution: dist,
-        path: [buildInfo.srcVendors, pack].join('/'),
+        path: [buildInfo.srcVendors, pack].join('/'),        
+        smallPath: pack,
         styles: vendors.styles,
         static: vendors.static,
         templates: vendors.templates,
@@ -104,6 +105,7 @@ runner.task('vendors', 'Generate vendors libraries', function(callback) {
     });
     
     pm.createDistInfo({
+      isVendor: true,
       name: dist,
       packages: distPackages,
       output: buildInfo.vendors.output
@@ -131,37 +133,81 @@ runner.task('task:clean', 'Clean all generated directories', function(callback) 
   
 });
 
-runner.task('task:configure', 'Retrieve configuration parameters', function(callback) {
-  var self = this;
+runner.task('task:configure', 'Retrieve configuration parameters', ['task:checkConfig'], function(callback) {
+  var self = this, key, devBuildInfo;
   
   fs.readFile([__dirname, 'ember_runner_config.json'].join('/'), function(err, file) {
     if (err) return callback("Error no ember_runner_config.json file found");
     
     buildInfo = JSON.parse(file);
     
-    // Generate generics attributes
-    buildInfo.srcApps = [workDir, buildInfo.apps.input].join('/');
-    buildInfo.srcVendors = [workDir, buildInfo.vendors.input].join('/');
-    buildInfo.tmpApps = [workDir, buildInfo.tmpDir, buildInfo.apps.input].join('/');
-    buildInfo.tmpVendors = [workDir, buildInfo.tmpDir, buildInfo.vendors.input].join('/');
-    buildInfo.tgtApps = [workDir, buildInfo.apps.output].join('/');
-    buildInfo.tgtVendors = [workDir, buildInfo.vendors.output].join('/');
+    fs.readFile([workDir, 'ember_runner_config.json'].join('/'), function(err, file) {
+      if (err) return callback("Error no ember_runner_config.json file found");
 
-    // Generate apps distribution if not found
-    if (!buildInfo.apps.distributions) {
-      buildInfo.apps.distributions = {};
-      fs.readdir(buildInfo.srcApps, function(err, lists) {
-        if (err) return callback("Error retrieing apps names");
+      devBuildInfo = JSON.parse(file);
+    
+      // merge devBuildInfo keys with buildInfo
+      
+      // first apps
+      if (devBuildInfo.apps) {
+        for(key in devBuildInfo.apps) { 
+          buildInfo.apps[key] = devBuildInfo.apps[key]; 
+        }
+      }
+      
+      // then vendors without distribution
+      if (devBuildInfo.vendors) {
+        for(key in devBuildInfo.vendors) { 
+          if (key != 'distributions') buildInfo.vendors[key] = devBuildInfo.vendors[key];
+        }
+      }
+      
+      // vendor's distribution
+      if (devBuildInfo.vendors.distributions) {
+        for(key in devBuildInfo.vendors.distributions) { 
+          buildInfo.vendors.distributions[key] = devBuildInfo.vendors.distributions[key];
+        }
+      }
+      
+      // server without proxies
+      if (devBuildInfo.server) {
+        for(key in devBuildInfo.server) { 
+          if (key != 'proxy') buildInfo.server[key] = devBuildInfo.server[key];
+        }
+      }
+      
+      // server's proxy
+      if (devBuildInfo.server.proxy) {
+        for(key in devBuildInfo.server.proxy) { 
+          buildInfo.server.proxy[key] = devBuildInfo.server.proxy[key];
+        }
+      }
+      
+      // Generate generics attributes
+      buildInfo.srcApps = [workDir, buildInfo.apps.input].join('/');
+      buildInfo.srcVendors = [workDir, buildInfo.vendors.input].join('/');
+      buildInfo.tmpApps = [workDir, buildInfo.tmpDir, buildInfo.apps.input].join('/');
+      buildInfo.tmpVendors = [workDir, buildInfo.tmpDir, buildInfo.vendors.input].join('/');
+      buildInfo.tgtApps = [workDir, buildInfo.apps.output].join('/');
+      buildInfo.tgtVendors = [workDir, buildInfo.vendors.output].join('/');
+
+      // Generate apps distribution if not found
+      if (!buildInfo.apps.distributions) {
+        buildInfo.apps.distributions = {};
+        fs.readdir(buildInfo.srcApps, function(err, lists) {
+          if (err) return callback("There no applications found, nothing to do.\nCreate one with ember-runner -g app <your app>");
         
-        lists.forEach(function(list) {
-          buildInfo.apps.distributions[list] = [list];
+          lists.forEach(function(list) {
+            buildInfo.apps.distributions[list] = [list];
+          });
+        
+          callback(null, true);        
         });
-        
-        callback(null, true);        
-      });
-    } else {
-      callback(null, true);
-    }    
+      } else {
+        callback(null, true);
+      }
+      
+    });      
   });
 });
 
@@ -191,4 +237,29 @@ runner.task('task:walk', 'Check for all files to be used', function(callback) {
     callback(null, true);
   });
   
+});
+
+runner.task('task:checkConfig', 'Check if the working directory is a valid ember-runner project', function(callback) {
+  fs.stat('ember_runner_config.json', function(err, stat) {
+    if (err) return callback("This is not a valid ember-runner project, no ember_runner_config.json found.\nGenerate one with: ember-runner --initialize/-i", true);
+    callback(null, true);
+  });
+});
+  
+runner.task('task:checkPackages', 'Retrieve configuration parameters', function(callback) {
+  var packages = pm.packages, errors = [], pending;
+  
+  if (!packages || packages.length === 0) return callback('No packages found (apps and vendors), nothing to do.');
+  
+  pending = packages.length;
+  
+  packages.forEach(function(pack) {
+    fs.stat(pack.path, function(err) {
+      if (err) errors.push('Package: ' + pack.name +' ([' + (pack.isVendor ? 'vendors' : 'apps') +'/]' + pack.smallPath + ') not found, please add it in vendors');
+      if (--pending === 0) {
+        if (errors.length > 0) return callback(errors.join('\n'));
+        callback(null, true);
+      }
+    });
+  });
 });
